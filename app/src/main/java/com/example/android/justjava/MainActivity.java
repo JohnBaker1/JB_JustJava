1ec1f5a6f5e7f03fb9d98bae5e23aa8f979eeb1f
John Baker <jbaker@student.sjcny.edu>
2017-02-10 21:12:32 -0500

    Initial commit

---
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.android.justjava;
+
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+import android.icu.text.NumberFormat;
+
+/**
+ * This app displays an order form to order coffee.
+ */
+public class MainActivity extends AppCompatActivity {
+    int count = 0;
+    int price = 5; //in dollars
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    /**
+     * This method is called when the order button is clicked.
+     */
+    public void increment(View view){
+        count++;
+        update();
+    }
+    public void decrement(View view){
+        if(count>0) {
+            count--;
+            update();
+        }
+    }
+
+    public void submitOrder(View view) {
+        displayPrice(count*price);
+    }
+    private void update(){
+        display(count);
+        displayPrice(0);
+    }
+
+    /**
+     * This method displays the given quantity value on the screen.
+     */
+    private void display(int number) {
+        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+        quantityTextView.setText("" + number);
+    }
+    private void displayPrice(int number) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        priceTextView.setText("Total: "+NumberFormat.getCurrencyInstance().format(number));
+
+    }
+}
